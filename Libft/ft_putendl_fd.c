89d94ca038a85eeb532770dff024c8586dd87f0c
Frank Person <miguelrapphenriques@gmail.com>
2024-11-22 21:29:19 +0000

    There it goes

--- Libft/ft_putendl_fd.c
+++ Libft/ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: mrapp-he <mrapp-he@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 20:47:58 by mrapp-he          #+#    #+#             */
-/*   Updated: 2024/11/22 00:12:45 by mrapp-he         ###   ########.fr       */
+/*   Updated: 2024/11/22 21:29:14 by mrapp-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	ft_putendl_fd(char *str, int fd)
 {
+	if (!str)
+		return ;
 	ft_putstr_fd(str, fd);
 	ft_putchar_fd('\n', fd);
 }
--- Libft/ft_putstr_fd.c
+++ Libft/ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: mrapp-he <mrapp-he@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 20:42:56 by mrapp-he          #+#    #+#             */
-/*   Updated: 2024/11/22 00:12:24 by mrapp-he         ###   ########.fr       */
+/*   Updated: 2024/11/22 21:28:50 by mrapp-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 void	ft_putstr_fd(char *str, int fd)
 {
+	if (!str)
+		return ;
 	write(fd, str, ft_strlen(str));
 }
