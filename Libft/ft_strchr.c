8004ec363c407f00133a4a3693ef7c5c9b5c4734
Frank Person <miguelrapphenriques@gmail.com>
2024-11-22 21:25:54 +0000

    There it goes

--- Libft/ft_strchr.c
+++ Libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: mrapp-he <mrapp-he@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 18:42:03 by mrapp-he          #+#    #+#             */
-/*   Updated: 2024/11/21 02:46:49 by mrapp-he         ###   ########.fr       */
+/*   Updated: 2024/11/22 21:25:05 by mrapp-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 char	*ft_strchr(const char *str, int c)
 {
-	if (!str)
-		return (NULL);
 	while (*str)
 	{
 		if (*str == (unsigned char)c)
