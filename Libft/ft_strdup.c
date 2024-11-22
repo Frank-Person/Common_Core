375c5614f48c277977fb4b5d961f1fd04fc0df96
Frank Person <miguelrapphenriques@gmail.com>
2024-11-22 21:28:07 +0000

    There it goes

--- Libft/ft_strdup.c
+++ Libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: mrapp-he <mrapp-he@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 23:43:38 by mrapp-he          #+#    #+#             */
-/*   Updated: 2024/11/21 02:45:49 by mrapp-he         ###   ########.fr       */
+/*   Updated: 2024/11/22 21:27:58 by mrapp-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ char	*ft_strdup(const char *str)
 	int		i;
 	char	*dup;
 
-	if (!str)
-		return (NULL);
 	i = -1;
 	dup = malloc((ft_strlen(str) + 1) * sizeof(char));
 	if (!dup)
