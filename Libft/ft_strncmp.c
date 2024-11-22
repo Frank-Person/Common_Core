b5b30c9266655c5bcb653795554f573f8a1edf25
Frank Person <miguelrapphenriques@gmail.com>
2024-11-22 21:27:12 +0000

    There it goes

--- Libft/ft_strncmp.c
+++ Libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mrapp-he <mrapp-he@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 20:27:58 by mrapp-he          #+#    #+#             */
-/*   Updated: 2024/11/22 00:02:44 by mrapp-he         ###   ########.fr       */
+/*   Updated: 2024/11/22 21:26:59 by mrapp-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	if (!s1 || !s2)
-		return (0);
 	while (*s1 && *s2 && *s1 == *s2 && n--)
 	{
 		s1++;
