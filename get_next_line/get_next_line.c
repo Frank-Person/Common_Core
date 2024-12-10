d0aa069652b25b43dcba914577a4fd28ec0c83c8
Frank Person <miguelrapphenriques@gmail.com>
2024-12-10 14:55:58 +0000

    some changes made

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,11 +6,23 @@
 /*   By: mrapp-he <mrapp-he@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/01 17:19:31 by mrapp-he          #+#    #+#             */
-/*   Updated: 2024/12/09 23:34:14 by mrapp-he         ###   ########.fr       */
+/*   Updated: 2024/12/10 14:39:43 by mrapp-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	if (!str)
+		return (0);
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
 
 int	ch_newline(char *buffer)
 {
@@ -36,7 +48,7 @@ char	*get_next_line(int fd)
 	{
 		if (!buffer[0])
 			rd = read(fd, buffer, BUFFER_SIZE);
-		if (rd < 0)
+		if (rd < 1)
 		 	return (free(line), NULL);
 		line = line_join(line, buffer);
 		if (ch_newline(buffer) || rd == 0)
@@ -50,15 +62,16 @@ char	*get_next_line(int fd)
 int	main(void)
 {
 	int	fd;
+	int	stp;
 	char	*line;
 	
 	fd = open("test.txt", O_RDONLY);
-	while (line)
+	stp = 1;
+	while (stp)
 	{
 		line = get_next_line(fd);
-		if (!line)
-			break ;
-		printf("%s", line);
+		stp = write(1, line, ft_strlen(line));
+		free(line);
 	}
-	return 0;
+	return (0);
 }
