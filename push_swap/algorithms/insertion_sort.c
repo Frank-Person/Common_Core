d1096105dae9002615070a41397057933946d9dc
Miguel Rapp Henriques <mrapp-he@c2r5s8.42lisboa.com>
2025-03-05 17:04:56 +0000

    Sending

--- push_swap/algorithms/insertion_sort.c
+++ push_swap/algorithms/insertion_sort.c
@@ -6,13 +6,16 @@
 /*   By: mrapp-he <mrapp-he@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 20:50:14 by mrapp-he          #+#    #+#             */
-/*   Updated: 2025/03/04 15:54:04 by mrapp-he         ###   ########.fr       */
+/*   Updated: 2025/03/05 16:54:26 by mrapp-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-t_list	*insert_sort(t_list **stack)
+t_list	*insert_sort(t_list *head)
 {
+	t_list	*dummy_node;
+
+	dummy_node->next = head;
 	
 }
--- push_swap/algorithms/quick_sort.c
+++ push_swap/algorithms/quick_sort.c
@@ -6,18 +6,15 @@
 /*   By: mrapp-he <mrapp-he@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 20:49:43 by mrapp-he          #+#    #+#             */
-/*   Updated: 2025/03/04 15:58:37 by mrapp-he         ###   ########.fr       */
+/*   Updated: 2025/03/05 16:56:41 by mrapp-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-t_list	*quick_sort(t_list **stack)
+t_list	*quick_sort(t_list *head)
 {
 	t_list	*dummy_node;
-	t_list	*pivot;
 
-	dummy_node->next = (*stack);
-	pivot = ft_lstlast(*stack);
-	
+	dummy_node->next = head;
 }
