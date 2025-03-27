6fbe1a8442a167d4d794622392dd9aaad785da8e
frank-person <miguelrapphenriques@gmail.com>
2025-03-27 16:56:36 +0000

    Sending

---
+++ fract-ol/fractol.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrapp-he <mrapp-he@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/27 16:14:35 by mrapp-he          #+#    #+#             */
+/*   Updated: 2025/03/27 16:23:13 by mrapp-he         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <string.h>
+# include <stdio.h>
+# include <math.h>
+
+
+
+#endif
