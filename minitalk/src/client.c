4a4e212af4693f7244b37beed2fd1aa91b6897a9
frank-person <miguelrapphenriques@gmail.com>
2025-04-11 17:11:02 +0100

    Sending

--- minitalk/src/client.c
+++ minitalk/src/client.c
@@ -24,60 +24,70 @@ static void	sig_receiver(int sig)
 	if (sig == SIGUSR1)
 		db()->ack = 1;
 	else if (sig == SIGUSR2)
+	{
+		db()->ack = 1;
 		ft_printf("Message Received!\n");
+	}
 }
 
-static void	size_handler(char *str)
+static void	send_size(void)
 {
 	db()->bit = 0;
-	db()->size = ft_strlen(str);
 	while (db()->bit < 32)
 	{
-		db()->ack = 0;
 		if (db()->size & (1 << db()->bit))
-			kill(db()->pid, SIGUSR1);
+			db()->bin = kill(db()->pid, SIGUSR1);
 		else
-			kill(db()->pid, SIGUSR2);
+			db()->bin = kill(db()->pid, SIGUSR2);
+		if (db()->bin == -1)
+			exit(ft_printf("Invalid PID\n"));
 		while (!db()->ack)
 			usleep(100);
 		db()->bit++;
+		db()->ack = 0;
 	}
 }
 
-static void	sig_handler(char c)
+static void	send_str(void)
 {
-	db()->bit = 0;
-	while (db()->bit < 8)
+	db()->size = ft_strlen(db()->str);
+	send_size();
+	while (db()->index <= db()->size)
 	{
-		db()->ack = 0;
-		if (c & (1 << db()->bit))
-			kill(db()->pid, SIGUSR1);
-		else
-			kill(db()->pid, SIGUSR2);
-		while (!db()->ack)
-			usleep(100);
-		db()->bit++;
+		db()->bit = 0;
+		while (db()->bit < 8)
+		{
+			if (db()->str[db()->index] & (1 << db()->bit))
+				kill(db()->pid, SIGUSR1);
+			else
+				kill(db()->pid, SIGUSR2);
+			while (!db()->ack)
+				usleep(100);
+			db()->bit++;
+			db()->ack = 0;
+		}
+		db()->index++;
 	}
+	db()->size = 0;
 }
 
 int	main(int ac, char **av)
 {
 	if (ac == 3)
 	{
-		while (*av[1] && (*av[1] >= '0' && *av[1] <= '9'))
+		if (*av[1] && *av[1] == '+')
+			av[1]++;
+		while (*av[1] >= '0' && *av[1] <= '9')
 			db()->pid = (db()->pid * 10) + (*av[1]++ - '0');
-		if (db()->pid == 0 || kill(db()->pid, 0))
+		if (db()->pid == 0)
 			exit(ft_printf("Invalid PID\n"));
+		db()->str = av[2];
 		signal(SIGUSR2, sig_receiver);
 		signal(SIGUSR1, sig_receiver);
-		while (*av[2])
-		{
-			if (db()->size == 0)
-				size_handler(av[2]);
-			sig_handler(*av[2]++);
-		}
-		if (db()->size != 0)
-			sig_handler('\0');
+		if (!*av[2])
+			send_size();
+		else
+			send_str();
 	}
 	else
 		exit(ft_printf("Error\n"));
--- minitalk/src/server.c
+++ minitalk/src/server.c
@@ -24,22 +24,29 @@ static void	msg_received(int pid, int size_check)
 	kill(pid, SIGUSR2);
 	if (size_check)
 	{
-		db()->str[db()->index] = '\0';
+		db()->str[db()->index - 1] = '\0';
 		ft_printf("%s\n", db()->str);
 		free(db()->str);
 	}
 	db()->size = 0;
 	db()->index = 0;
+	db()->bit = 0;
+	db()->bin = 0;
 }
 
-static void	set_size(int pid)
+static void	alloc_str(int pid)
 {
 	db()->size = db()->bin;
-	if (db()->size == 0)
-		msg_received(pid, 0);
-	db()->index = -1;
 	db()->bin = 0;
 	db()->bit = 0;
+	if (db()->size == 0)
+	{
+		msg_received(pid, 0);
+		return ;
+	}
+	db()->str = malloc((db()->size + 1) * sizeof(char));
+	if (!db()->str)
+		return ;
 }
 
 static void	sig_handler(int sig, siginfo_t *to_handle, void *trash)
@@ -49,21 +56,15 @@ static void	sig_handler(int sig, siginfo_t *to_handle, void *trash)
 		db()->bin |= (1 << db()->bit);
 	db()->bit++;
 	if (db()->bit == 32 && db()->size == 0)
+		alloc_str(to_handle->si_pid);
+	if (db()->bit == 8 && db()->size != 0 && db()->str)
 	{
-		set_size(to_handle->si_pid);
-		if (db()->size != 0)
+		db()->str[db()->index++] = db()->bin;
+		if (db()->bin == 0)
 		{
-			db()->str = malloc((db()->size + 1) * sizeof(char));
-			if (!db()->str)
-				return ;
-		}
-	}
-	if (db()->bit == 8 && db()->size != 0)
-	{
-		if (++db()->index < db()->size)
-			db()->str[db()->index] = db()->bin;
-		if (db()->bin == 0 && db()->size != 0)
 			msg_received(to_handle->si_pid, 1);
+			return ;
+		}
 		db()->bin = 0;
 		db()->bit = 0;
 	}
